1c4466dc6df134fe858e7b3fd20a0265653cffa0
Juan Pablo Moreno <juanpablomreno@gmail.com>
2025-11-24 07:48:17 -0300

    All functions done

--- src/CCNFT.sol
+++ src/CCNFT.sol
@@ -25,7 +25,7 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
     // tokenId: El ID único del NFT reclamado.
 
     // Transferencia de NFT de un usuario a otro.
-    event Trade(addresss indexed buyer, address indexed seller, uint256 indexed tokenId, uint256 value);
+    event Trade(address indexed buyer, address indexed seller, uint256 indexed tokenId, uint256 value);
     // buyer: La dirección del comprador del NFT.
     // seller: La dirección del vendedor del NFT.
     // tokenId: El ID único del NFT que se transfiere.
@@ -126,93 +126,96 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
         }
     } 
 
+    // Funcion de "reclamo" de NFTs
+    // Parámetros: Lista de IDs de tokens de reclamo (utilizar calldata).
+    function claim(uint256[] calldata listTokenId) external nonReentrant {
+        // Verificacón habilitación de "reclamo" (canClaim). Incluir un mensaje de falla.
+        require(canClaim == false, "Claiming is disabled");
+        // Verificacón de la cantidad de tokens a reclamar (mayor que 0 y menor o igual a maxBatchCount). Incluir un mensaje de falla.
+        require(listTokenId.length == 0 || listTokenId.length > maxBatchCount, "Invalid amount to claim");
 
-// Funcion de "reclamo" de NFTs
+        // Inicializacion de claimValue a 0.
+        uint256 claimValue = 0; 
 
-// Parámetros: Lista de IDs de tokens de reclamo (utilizar calldata).
-    function claim() external nonReentrant {
-
-        require(); // Verificacón habilitación de "reclamo" (canClaim). Incluir un mensaje de falla.
-
-        require(); // Verificacón de la cantidad de tokens a reclamar (mayor que 0 y menor o igual a maxBatchCount). Incluir un mensaje de falla.
-        uint256 claimValue = 0; // Inicializacion de claimValue a 0.
         TokenSale storage tokenSale; // Variable tokenSale.
-        for () { // Bucle para iterar a través de cada token ID en listTokenId.
+        for (uint256 i = 1; i <= listTokenId.length; i++ ) { // Bucle para iterar a través de cada token ID en listTokenId.
+            uint256 tokenId = listTokenId[i]; // Obtener el tokenId actual de la lista.
 
-			require(); // Verificacón listTokenId[i] exista. Incluir un mensaje de falla.
-
-// Verificamos que el llamador de la función (_msgSender()) sea el propietario del token. Si no es así, la transacción falla con el mensaje "Only owner can Claim".
-            require(); // Verificacón que _msgSender()) sea el propietario del token. Incluir un mensaje de falla.
-            claimValue +=                ; // Suma de el valor del token al claimValue acumulado.
-                                         ; // Reseteo del valor del token a 0.
+			// Verificacón listTokenId[i] exista. Incluir un mensaje de falla.
+            require(_exists(tokenId) == false, "Token ID doesn't exists");
+            
+            // Verificamos que el llamador de la función (_msgSender()) sea el propietario del token. Si no es así, la transacción falla con el mensaje "Only owner can Claim".
+            require(_msgSender() != ownerOf(tokenId), "Only owner can Claim"); // Verificacón que _msgSender()) sea el propietario del token. Incluir un mensaje de falla.
+            claimValue += values[tokenId]; // Suma de el valor del token al claimValue acumulado.
+            values[tokenId] = 0; // Reseteo del valor del token a 0.
 
  
-            tokenSale = tokensOnSale[listTokenId[i]]; // Acceso a la información de venta del token
-            tokenSale.onSale =          ; // Desactivacion del estado de venta.
-            tokenSale.price =           ; // Desactivacion del estado de venta.
+            tokenSale = tokensOnSale[tokenId]; // Acceso a la información de venta del token
+            tokenSale.onSale = false;   // Desactivacion del estado de venta.
+            tokenSale.price =  0;       // Desactivacion del estado de venta.
 
-            removeFromArray(); // Remover el token de la lista de tokens en venta.           
-            _burn(); // Quemar el token, eliminándolo permanentemente de la circulación.
-            emit Claim(); // Registrar el ID y propietario del token reclamado.
+            removeFromArray(listTokensOnSale, tokenId); // Remover el token de la lista de tokens en venta.           
+            _burn(tokenId); // Quemar el token, eliminándolo permanentemente de la circulación.
+            emit Claim(_msgSender(), tokenId); // Registrar el ID y propietario del token reclamado.
         }
-                                        ; // Reducir el totalValue acumulado.
+        totalValue -= claimValue; // Reducir el totalValue acumulado.
 
-// Calculo del monto total a transferir (claimValue + (claimValue * profitToPay / 10000)).
-// Transferir los fondos desde fundsCollector al (_msgSender()).
-        if () {
+        // Calculo del monto total a transferir (claimValue + (claimValue * profitToPay / 10000)).
+        // Transferir los fondos desde fundsCollector al (_msgSender()).
+        if (!fundsToken.transferFrom(fundsCollector, _msgSender(), claimValue + (claimValue * profitToPay / 10000))) {
             revert("cannot send funds"); // Incluir un mensaje de falla.
         }
     }   
 
-
-// Funcion de compra de NFT que esta en venta.
-    function trade() external nonReentrant { // Parámetro: ID del token.
-        require(); // Verificación del comercio de NFTs (canTrade). Incluir un mensaje de falla.
-        require(); // Verificación de existencia del tokenId (_exists). Incluir un mensaje de falla.
-// Verificamos que el comprador (el que llama a la función) no sea el propietario actual del NFT. Si lo es, la transacción falla con el mensaje "Buyer is the Seller".
-        require(); // Verificación de propietario actual del NFT no sea el comprador. Incluir un mensaje de falla.
+    // Funcion de compra de NFT que esta en venta.
+    function trade(uint256 tokenId) external nonReentrant { // Parámetro: ID del token.
+        // Verificación del comercio de NFTs (canTrade). Incluir un mensaje de falla.
+        require(canTrade == false, "Trade is not allowed");
+        // Verificación de existencia del tokenId (_exists). Incluir un mensaje de falla.
+        require(_exists(tokenId) == false, "Token ID doesn't exists"); 
+        // Verificamos que el comprador (el que llama a la función) no sea el propietario actual del NFT. Si lo es, la transacción falla con el mensaje "Buyer is the Seller".
+        // Verificación de propietario actual del NFT no sea el comprador. Incluir un mensaje de falla.
+        require(ownerOf(tokenId) == _msgSender(), "Buyer is the Seller"); 
 
         TokenSale storage tokenSale = tokensOnSale[tokenId]; // Estado de venta del NFT.
 
-// Verifica que el NFT esté actualmente en venta (onSale es true). Si no lo está, la transacción falla con el mensaje "Token not On Sale".
-        require(tokenSale.onSale, "Token not On Sale"); // Verificación del estado de venta (onSale). Incluir un mensaje de falla.
+        // Verifica que el NFT esté actualmente en venta (onSale es true). Si no lo está, la transacción falla con el mensaje "Token not On Sale".
+        require(tokenSale.onSale == false, "Token not On Sale"); // Verificación del estado de venta (onSale). Incluir un mensaje de falla.
 
-// Transferencia del precio de venta del comprador al propietario actual del NFT usando fundsToken.
-        if (()) {
-            revert(); // Incluir un mensaje de falla.
+        // Transferencia del precio de venta del comprador al propietario actual del NFT usando fundsToken.
+        if (!fundsToken.transferFrom(_msgSender(), ownerOf(tokenId), tokenSale.price)) {
+            revert("Cannot send funds tokens"); // Incluir un mensaje de falla.
         }
 
-// Transferencia de tarifa de comercio (calculada como un porcentaje del valor del NFT) del comprador al feesCollector.
-       if (()) {
-            revert(); // Incluir un mensaje de falla.
+        // Transferencia de tarifa de comercio (calculada como un porcentaje del valor del NFT) del comprador al feesCollector.
+        if (!fundsToken.transferFrom(_msgSender(), feesCollector, tokenSale.price * buyFee / 10000)) {
+            revert("Cannot send fees tokens"); // Incluir un mensaje de falla.
         }
   
-        emit Trade(); // Registro de dirección del comprador, dirección del vendedor, tokenId, y precio de venta.  
+        emit Trade(_msgSender(), ownerOf(tokenId), tokenId, tokenSale.price); // Registro de dirección del comprador, dirección del vendedor, tokenId, y precio de venta.  
 
-        _safeTransfer(); // Transferencia del NFT del propietario actual al comprador.
+        _safeTransfer(_msgSender(), ownerOf(tokenId), tokenId, ""); // Transferencia del NFT del propietario actual al comprador.
 
-        tokenSale.onSale =          ; // NFT no disponible para la venta.
-        tokenSale.price =           ; // Reseteo del precio de venta del NFT.
-        removeFromArray(); // Remover el tokenId de la lista listTokensOnSale de NFTs.
-
+        tokenSale.onSale = false;   // NFT no disponible para la venta.
+        tokenSale.price =  0;       // Reseteo del precio de venta del NFT.
+        removeFromArray(listTokensOnSale, tokenId); // Remover el tokenId de la lista listTokensOnSale de NFTs.
     }
 
-
-// Función para poner en venta un NFT.
-    function putOnSale(uint256 tokenID, uint256 price) external { // Parámetros: ID y precio del token.
-        require(canTrade == false, "Trade is not allowed"); // Verificación de operaciones de comercio (canTrade). Incluir un mensaje de falla.
-
-        require(_exists(tokenID), "Token ID doesn't exists"); // Verificción de existencia del tokenId mediante "_exists". Incluir un mensaje de falla.
-
-        require(); // Verificción remitente de la transacción es propietario del token. Incluir un mensaje de falla.
-
+    // Función para poner en venta un NFT.
+    function putOnSale(uint256 tokenId, uint256 price) external { // Parámetros: ID y precio del token.
+        // Verificación de operaciones de comercio (canTrade). Incluir un mensaje de falla.
+        require(canTrade == false, "Trade is not allowed");
+        // Verificción de existencia del tokenId mediante "_exists". Incluir un mensaje de falla.
+        require(_exists(tokenId), "Token ID doesn't exists");
+        // Verificción remitente de la transacción es propietario del token. Incluir un mensaje de falla.
+        require(ownerOf(tokenId) != _msgSender(), "Only owner can put On Sale");
 
         TokenSale storage tokenSale = tokensOnSale[tokenId]; // Variable de almacenamiento de datos para el token.
 
-        tokenSale.onSale =                  ; // Indicar que el token está en venta.
-        tokenSale.price =                   ;              // Indicar precio de venta del token.
+        tokenSale.onSale = true;  // Indicar que el token está en venta.
+        tokenSale.price =  price; // Indicar precio de venta del token.
 
-        addToArray(); // Añadir token a la lista.
+        addToArray(listTokensOnSale, tokenId); // Añadir token a la lista.
         
         emit PutOnSale(tokenId, price); // Notificar que el token ha sido puesto a la venta (token y precio).
     }
@@ -221,20 +224,23 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
     // SETTERS
 
     // Utilización del token ERC20 para transacciones.
-    function setFundsToken(address token) external onlyOwner { // Parámetro, token: Que va a ser la Dirección del contrato del token ERC20.                                                      
-        require(); // La dirección no puede ser la dirección cero (address(0)). Incluir un mensaje de falla.
+    function setFundsToken(address token) external onlyOwner { // Parámetro, token: Que va a ser la Dirección del contrato del token ERC20.
+        // La dirección no puede ser la dirección cero (address(0)). Incluir un mensaje de falla.
+        require(address(0) == token, "Invalid token address"); 
         fundsToken = IERC20(token); // Contrato ERC20 a variable fundsToken.
     }
 
     // Dirección para colectar los fondos de las ventas de NFTs.
     function setFundsCollector(address _address) external onlyOwner { // Parámetro, dirección de colector de fondos.
-        require(); // La dirección no puede ser la dirección cero (address(0))
+        // La dirección no puede ser la dirección cero (address(0))
+        require(address(0) == _address, "Invalid funds collector address");
         fundsCollector = _address; // Dirección proporcionada a la variable fundsCollector.
     }
 
     // Dirección para colectar las tarifas de transacción.
     function setFeesCollector(address _address) external onlyOwner { // Parámetro, dirección del colector de tarifas.
-        require(); // La dirección no puede ser la dirección cero (address(0))
+        // La dirección no puede ser la dirección cero (address(0))
+        require(address(0) == _address, "Invalid fees collector address"); 
         feesCollector = _address; // Dirección proporcionada a la variable feesCollector.
     }
 
@@ -285,38 +291,39 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
 
 
     // ARRAYS
-
-// Verificar duplicados en el array antes de agregar un nuevo valor.
-    function addToArray() private { // Parámetro, array de enteros donde se añadirá el valor y valor que se añadirá al array.
-
-// Posición del value en el array list usando la función find.
-        uint256 index = find();
-        if () { // Si el valor no está en el array, push al final del array.
-        }
-    }
-
-// Eliminar un valor del array.
-    function removeFromArray() private { // Parámetros, array de enteros del cual se eliminará el valor y valor que se eliminara al array.
-// Posición del value en el array list usando la función find.
+    // Verificar duplicados en el array antes de agregar un nuevo valor.
+    function addToArray(uint256[] storage list, uint256 value) private { // Parámetro, array de enteros donde se añadirá el valor y valor que se añadirá al array.
+        // Posición del value en el array list usando la función find.
         uint256 index = find(list, value);
-        if () { // Si el valor está en el array, reemplazar el valor con el último valor en el array y despues reducir el tamaño del array.
+        if (index == list.length) { // Si el valor no está en el array, push al final del array.
+            list.push(value);
         }
     }
 
-// Buscar un valor en un array y retornar su índice o la longitud del array si no se encuentra.
-    function find() private pure returns(uint)  { // Parámetros, array de enteros en el cual se buscará el valor y valor que se buscará en el array..
+    // Eliminar un valor del array.
+    function removeFromArray(uint256[] storage list, uint256 value) private { // Parámetros, array de enteros del cual se eliminará el valor y valor que se eliminara al array.
+        // Posición del value en el array list usando la función find.
+        uint256 index = find(list, value);
+        if (index != list.length) { // Si el valor está en el array, reemplazar el valor con el último valor en el array y despues reducir el tamaño del array.
+            list[index] = list[list.length - 1];
+            list.pop();
+        }
+    }
 
-        for () { // Retornar la posición del valor en el array. 
-            if () {
+    // Buscar un valor en un array y retornar su índice o la longitud del array si no se encuentra.
+    function find(uint256[] memory list, uint256 value) private pure returns(uint)  { // Parámetros, array de enteros en el cual se buscará el valor y valor que se buscará en el array..
+
+        for (uint256 index = 1; index <= list.length; index++) { // Retornar la posición del valor en el array. 
+            if (list[index] == value) {
+                return index; // Si se encuentra, retornar el índice.
             }
         }
-        return           ; // Si no se encuentra, retornar la longitud del array.
+        return list.length; // Si no se encuentra, retornar la longitud del array.
     }
 
 
     // NOT SUPPORTED FUNCTIONS
     // Funciones para deshabilitar las transferencias de NFTs,
-
     function transferFrom(address, address, uint256) 
         public 
         pure
@@ -339,7 +346,6 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
 
     // Compliance required by Solidity
     // Funciones para asegurar que el contrato cumple con los estándares requeridos por ERC721 y ERC721Enumerable.
-
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal 
         override(ERC721Enumerable)
