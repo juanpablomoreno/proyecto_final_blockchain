7043426774a40935e58722e604082c03eee7edec
juanpablomoreno <juanpablomoreno@gmail.com>
2025-11-24 02:57:43 -0300

    verificacion de flags e inicio de contadores

--- src/CCNFT.sol
+++ src/CCNFT.sol
@@ -1,29 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {
-    ERC721
-} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {
-    ERC721Enumerable
-} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import {
-    IERC20
-} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {
-    IERC721
-} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import {
-    Counters
-} from "@openzeppelin/contracts/utils/Counters.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {
-    ReentrancyGuard
-} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
-
 //EVENTOS
 // indexed: Permiten realizar búsquedas en los registros de eventos.
 
@@ -50,28 +36,30 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
 // tokenId: El ID único del NFT que se pone en venta.
 // price: El precio al cual se pone en venta el NFT (No indexed).
 
-// Estructura del estado de venta de un NFT.
+    // Estructura del estado de venta de un NFT.
     struct TokenSale {
         // Indicamos si el NFT está en venta.
+        bool onSale;
         // Indicamos el precio del NFT si está en venta.
+        uint256 price;
     }
 
-// Biblioteca Counters de OpenZeppelin para manejar contadores de manera segura.
+    // Biblioteca Counters de OpenZeppelin para manejar contadores de manera segura.
     using Counters for Counters.Counter; 
 
-// Contador para asignar IDs únicos a cada NFT que se crea.
+    // Contador para asignar IDs únicos a cada NFT que se crea.
     Counters.Counter private tokenIdTracker;
 
-// Mapeo del ID de un token (NFT) a un valor específico.
+    // Mapeo del ID de un token (NFT) a un valor específico.
     mapping(uint256 => uint256) public values;
 
-// Mapeo de un valor a un booleano para indicar si el valor es válido o no.
+    // Mapeo de un valor a un booleano para indicar si el valor es válido o no.
     mapping(uint256 => bool) public validValues;
 
-// Mapeo del ID de un token (NFT) a su estado de venta (TokenSale).
+    // Mapeo del ID de un token (NFT) a su estado de venta (TokenSale).
     mapping(uint256 => TokenSale) public tokensOnSale;
 
-// Lista que contiene los IDs de los NFTs que están actualmente en venta.
+    // Lista que contiene los IDs de los NFTs que están actualmente en venta.
     uint256[] public listTokensOnSale;
     
     address public fundsCollector; // Dirección de los fondos de las ventas de los NFTs
@@ -92,11 +80,13 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
     uint32 public profitToPay; // Porcentaje adicional a pagar en las reclamaciones.
 
 
-// Referencia al contrato ERC20 manejador de fondos. 
+    // Referencia al contrato ERC20 manejador de fondos. 
     IERC20 public fundsToken;
 
-// Constructor (nombre y símbolo del NFT).    
+    // Constructor (nombre y símbolo del NFT).    
     constructor() ERC721("CryptoCampo NFT", "CCNFT") {
+        tokenIdTracker.reset(); // Inicialización del contador de IDs de NFTs.
+        tokenIdTracker.increment(); // Iniciamos el contador en 1 (los IDs de los NFTs comienzan desde 1).
     }
 
     // PUBLIC FUNCTIONS
@@ -105,33 +95,33 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
 
 // Parametro value: El valor de cada NFT que se está comprando.
 // Parametro amount: La cantidad de NFTs que se quieren comprar.
-    function buy() external nonReentrant {
-        require(); // Verificación de permisos de la compra con "canBuy". Incluir un mensaje de falla.
-        
-// Verificacón de la cantidad de NFTs a comprar sea mayor que 0 y menor o igual al máximo permitido (maxBatchCount). Incluir un mensaje de falla.
-        require();
+    function buy(uint256 value, uint16 amount) external nonReentrant {
+        // Verificación de permisos de la compra con "canBuy". Incluir un mensaje de falla.
+        require(canBuy == false, "Buying is disabled");
+        // Verificacón de la cantidad de NFTs a comprar sea mayor que 0 y menor o igual al máximo permitido (maxBatchCount). Incluir un mensaje de falla.
+        require(amount == 0 || amount > maxBatchCount, "Invalid amount to buy");
+        // Verificación del valor especificado para los NFTs según los valores permitidos en validValues. Incluir un mensaje de falla.
+        require(value == 0 || validValues[value] == false, "Invalid value to buy"); 
+        // Verificacón del valor total después de la compra (no debe exeder el valor máximo permitido "maxValueToRaise"). Incluir un mensaje de falla.
+        require(((value * amount) + totalValue) > maxValueToRaise, "Max value to raise exceeded");
 
-        require(); // Verificación del valor especificado para los NFTs según los valores permitidos en validValues. Incluir un mensaje de falla.
+        totalValue += (value * amount); // Incremento del valor total acumulado por el valor de los NFTs comprados.
 
-// Verificacón del valor total después de la compra (no debe exeder el valor máximo permitido "maxValueToRaise"). Incluir un mensaje de falla.
-        require();
-
-        totalValue +=              ; // Incremento del valor total acumulado por el valor de los NFTs comprados.
-
-        for () { // Bucle desde 1 hasta amount (inclusive) para mintear la cantidad especificada de NFTs.
-            values[] = value; // Asignar el valor del NFT al tokenId actual "current()" en el mapeo values.
-            _safeMint(); // Minteo de NFT y asignación al msg.sender.
-            emit Buy(); // Evento Buy con el comprador, el tokenId y el valor del NFT.
-                                    ; // Incremento del contador tokenIdTracker (NFT deben tener un tokenId único).        
+        for (uint256 index = 1; index <= amount; index++) { // Bucle desde 1 hasta amount (inclusive) para mintear la cantidad especificada de NFTs.
+            uint256 tokenId = tokenIdTracker.current(); // Obtener el tokenId actual del contador tokenIdTracker.
+            values[tokenId] = value; // Asignar el valor del NFT al tokenId actual "current()" en el mapeo values.
+            _safeMint(msg.sender, tokenId); // Minteo de NFT y asignación al msg.sender.
+            emit Buy(msg.sender, tokenId, value); // Evento Buy con el comprador, el tokenId y el valor del NFT.
+            tokenIdTracker.increment(); // Incremento del contador tokenIdTracker (NFT deben tener un tokenId único).        
         }
 
-// Transfencia de fondos desde el comprador (_msgSender()) al recolector de fondos (fundsCollector) por el valor total de los NFTs comprados. 
+        // Transfencia de fondos desde el comprador (_msgSender()) al recolector de fondos (fundsCollector) por el valor total de los NFTs comprados. 
         if (!fundsToken.transferFrom(_msgSender(), fundsCollector, value * amount)) {
             revert("Cannot send funds tokens"); // Incluir un mensaje de falla.
         }
 
-// Transferencia de tarifas de compra desde el comprador (_msgSender()) al recolector de tarifas (feesCollector).
-// Tarifa = fracción del valor total de la compra (value * amount * buyFee / 10000).
+        // Transferencia de tarifas de compra desde el comprador (_msgSender()) al recolector de tarifas (feesCollector).
+        // Tarifa = fracción del valor total de la compra (value * amount * buyFee / 10000).
         if (!fundsToken.transferFrom(_msgSender(), feesCollector, value * amount * buyFee / 10000)) {
             revert("Cannot send fees tokens"); // Incluir un mensaje de falla.
         }
@@ -210,10 +200,10 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
 
 
 // Función para poner en venta un NFT.
-    function putOnSale() external { // Parámetros: ID y precio del token.
-        require(); // Verificación de operaciones de comercio (canTrade). Incluir un mensaje de falla.
+    function putOnSale(uint256 tokenID, uint256 price) external { // Parámetros: ID y precio del token.
+        require(canTrade == false, "Trade is not allowed"); // Verificación de operaciones de comercio (canTrade). Incluir un mensaje de falla.
 
-        require(); // Verificción de existencia del tokenId mediante "_exists". Incluir un mensaje de falla.
+        require(_exists(tokenID), "Token ID doesn't exists"); // Verificción de existencia del tokenId mediante "_exists". Incluir un mensaje de falla.
 
         require(); // Verificción remitente de la transacción es propietario del token. Incluir un mensaje de falla.
 
@@ -231,65 +221,65 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
 
     // SETTERS
 
-// Utilización del token ERC20 para transacciones.
-    function setFundsToken() external onlyOwner { // Parámetro, token: Que va a ser la Dirección del contrato del token ERC20.                                                      
+    // Utilización del token ERC20 para transacciones.
+    function setFundsToken(address token) external onlyOwner { // Parámetro, token: Que va a ser la Dirección del contrato del token ERC20.                                                      
         require(); // La dirección no puede ser la dirección cero (address(0)). Incluir un mensaje de falla.
         fundsToken = IERC20(token); // Contrato ERC20 a variable fundsToken.
     }
 
-// Dirección para colectar los fondos de las ventas de NFTs.
-    function setFundsCollector() external onlyOwner { // Parámetro, dirección de colector de fondos.
+    // Dirección para colectar los fondos de las ventas de NFTs.
+    function setFundsCollector(address _address) external onlyOwner { // Parámetro, dirección de colector de fondos.
         require(); // La dirección no puede ser la dirección cero (address(0))
         fundsCollector = _address; // Dirección proporcionada a la variable fundsCollector.
     }
 
-// Dirección para colectar las tarifas de transacción.
-    function setFeesCollector() external onlyOwner { // Parámetro, dirección del colector de tarifas.
+    // Dirección para colectar las tarifas de transacción.
+    function setFeesCollector(address _address) external onlyOwner { // Parámetro, dirección del colector de tarifas.
         require(); // La dirección no puede ser la dirección cero (address(0))
         feesCollector = _address; // Dirección proporcionada a la variable feesCollector.
     }
 
-// Porcentaje de beneficio a pagar en las reclamaciones.
-    function setProfitToPay() external onlyOwner { // Parámetro, porcentaje de beneficio a pagar.
+    // Porcentaje de beneficio a pagar en las reclamaciones.
+    function setProfitToPay(uint32 _profitToPay) external onlyOwner { // Parámetro, porcentaje de beneficio a pagar.
         profitToPay = _profitToPay; // Valor proporcionado a la variable profitToPay.
     }
 
-// Función que Habilita o deshabilita la compra de NFTs.
-    function setCanBuy() external onlyOwner { // Parámetro, booleano que indica si la compra está permitida.
+    // Función que Habilita o deshabilita la compra de NFTs.
+    function setCanBuy(bool _canBuy) external onlyOwner { // Parámetro, booleano que indica si la compra está permitida.
         canBuy = _canBuy;  // Valor proporcionado a la variable canBuy.
     }
 
-// Función que Habilita o deshabilita la reclamación de NFTs.
-    function setCanClaim() external onlyOwner { // Parámetro, booleano que indica si la reclamacion está permitida.
+    // Función que Habilita o deshabilita la reclamación de NFTs.
+    function setCanClaim(bool _canClaim) external onlyOwner { // Parámetro, booleano que indica si la reclamacion está permitida.
         canClaim = _canClaim; // Valor proporcionado a la variable canClaim.
     }
 
-// Función que Habilita o deshabilita el intercambio de NFTs.
-    function setCanTrade() external onlyOwner { // Parámetro, booleano que indica si la intercambio está permitido.
+    // Función que Habilita o deshabilita el intercambio de NFTs.
+    function setCanTrade(bool _canTrade) external onlyOwner { // Parámetro, booleano que indica si la intercambio está permitido.
         canTrade = _canTrade; // Valor proporcionado a la variable canTrade.
     }
 
-// Valor máximo que se puede recaudar de venta de NFTs.
-    function setMaxValueToRaise() external onlyOwner { // Parámetro, valor máximo a recaudar.
+    // Valor máximo que se puede recaudar de venta de NFTs.
+    function setMaxValueToRaise(uint256 _maxValueToRaise) external onlyOwner { // Parámetro, valor máximo a recaudar.
         maxValueToRaise = _maxValueToRaise; // Valor proporcionado a la variable maxValueToRaise.
     }
     
-// Función para agregar un valor válido para NFTs.   
-    function addValidValues() external onlyOwner { // Parámetro, valor que se quiere agregar como válido.
+    // Función para agregar un valor válido para NFTs.   
+    function addValidValues(uint256 value) external onlyOwner { // Parámetro, valor que se quiere agregar como válido.
         validValues[value] = true; // Valor como válido en el mapeo validValues.
     }
 
-// Función para establecer la cantidad máxima de NFTs por operación.
-    function setMaxBatchCount() external onlyOwner { // Parámetro, cantidad máxima de NFTs por operación.
+    // Función para establecer la cantidad máxima de NFTs por operación.
+    function setMaxBatchCount(uint16 _maxBatchCount) external onlyOwner { // Parámetro, cantidad máxima de NFTs por operación.
         maxBatchCount = _maxBatchCount; // Valor proporcionado a la variable maxBatchCount.
     }
 
-// Tarifa aplicada a las compras de NFTs.
-    function setBuyFee() external onlyOwner { // Parámetro, porcentaje de tarifa para compras.
+    // Tarifa aplicada a las compras de NFTs.
+    function setBuyFee(uint16 _buyFee) external onlyOwner { // Parámetro, porcentaje de tarifa para compras.
         buyFee = _buyFee; // Valor proporcionado a la variable buyFee.
     }
 
-// Tarifa aplicada a las transacciones de NFTs.
+    // Tarifa aplicada a las transacciones de NFTs.
     function setTradeFee(uint16 _tradeFee) external onlyOwner { // Parámetro, porcentaje de tarifa para transacciones.
         tradeFee = _tradeFee; // Valor proporcionado a la variable tradeFee.
     }
@@ -326,8 +316,7 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
 
 
     // NOT SUPPORTED FUNCTIONS
-
-// Funciones para deshabilitar las transferencias de NFTs,
+    // Funciones para deshabilitar las transferencias de NFTs,
 
     function transferFrom(address, address, uint256) 
         public 
@@ -336,13 +325,11 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
     {
         revert("Not Allowed");
     }
-
     function safeTransferFrom(address, address, uint256) 
         public pure override(ERC721, IERC721) 
     {
         revert("Not Allowed");
     }
-
     function safeTransferFrom(address, address, uint256,  bytes memory) 
         public 
         pure
@@ -351,10 +338,8 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
         revert("Not Allowed");
     }
 
-
     // Compliance required by Solidity
-
-// Funciones para asegurar que el contrato cumple con los estándares requeridos por ERC721 y ERC721Enumerable.
+    // Funciones para asegurar que el contrato cumple con los estándares requeridos por ERC721 y ERC721Enumerable.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal 
@@ -362,6 +347,5 @@ contract CCNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
     {
         super._beforeTokenTransfer(from, to, tokenId);
     }
-   
 }
 
